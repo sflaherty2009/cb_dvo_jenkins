92078ce8595dff4020c46dee70f900418dfef8f8
Ray Crawford <ray_crawford@trekbikes.com>
2016-11-10 10:22:16 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cb_dvo_jenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: cb_dvo_jenkins
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
