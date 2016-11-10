c8ed8078e24611a58ba1c3a47ae344ec9cbe5d65
Ray Crawford <ray_crawford@trekbikes.com>
2016-11-10 10:33:54 -0500

    chef cookbook generate modified to pass 'foodcritic . --epic-fail any -X test'.

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,10 @@
 name 'cb_dvo_jenkins'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Ray Crawford'
+maintainer_email 'ray_crawford@trekbikes.com'
 license 'all_rights'
-description 'Installs/Configures cb_dvo_jenkins'
-long_description 'Installs/Configures cb_dvo_jenkins'
+description 'Installs/Configures jenkins resources with a focus on DVO Chef Pipeline needs.'
+long_description 'See README.md'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/cb_dvo_jenkins/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/cb_dvo_jenkins' if respond_to?(:source_url)
+source_url 'https://bitbucket.org/trekbikes/cb_dvo_jenkins'
+issues_url 'https://bitbucket.org/trekbikes/cb_dvo_jeknins/issues?status=new&status=open'
