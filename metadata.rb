083a0903e905629b422b569f6e47665d738e6690
Scott Flaherty <scott_flaherty@trekbikes.com>
2018-01-08 07:26:50 -0600

    v1.4 set supports centos in metadata. set chef version in metadata.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'ray_crawford@trekbikes.com'
 license 'all_rights'
 description 'Installs/Configures jenkins resources with a focus on DVO Chef Pipeline needs.'
 long_description 'See README.md'
-version '0.1.3'
+version '0.1.4'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'centos'
 
 source_url 'https://bitbucket.org/trekbikes/cb_dvo_jenkins'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_jeknins/issues?status=new&status=open'
