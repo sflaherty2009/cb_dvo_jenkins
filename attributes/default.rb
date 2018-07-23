eeb59e6b3ea45324e4cf761be50964dbf6b59693
dreweasland <drew_easland@trekbikes.com>
2018-07-23 13:47:26 -0500

    reverted port to test issue

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['jenkins']['master']['version'] = '2.107.3'
 default['jenkins']['server']['url'] = 'http://jenkins-test:8080'
 # update port to run on 80 as opposed to 8080
 default['jenkins']['master']['host'] = 'localhost'
-default['jenkins']['master']['port'] = '80'
+default['jenkins']['master']['port'] = '8080'
 default['jenkins']['master']['endpoint'] = "http://#{node['jenkins']['master']['host']}:#{node['jenkins']['master']['port']}"
 # update timeout to 480 seconds
 default['jenkins']['executor']['timeout'] = 480
