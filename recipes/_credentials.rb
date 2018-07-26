953d1cd952d890179e493e8e2d4d8485c6de5857
Scott Flaherty <scott_flaherty@trekbikes.com>
2018-07-26 09:16:16 -0500

    added conBase credential to jenkins for use with ongoing builds.

--- recipes/_credentials.rb
+++ recipes/_credentials.rb
@@ -9,3 +9,9 @@ jenkins_password_credentials 'TrekDevOps' do
   description 'creds used for pulls/push to bitbucket'
   password    'WQrULM66cGyPyB'
 end
+
+jenkins_password_credentials 'conBase' do
+  id          'conBase'
+  description 'creds for use by base container storage'
+  password    'NrD6bvZ4w7Xjk0TA6KYkfT5se/kiQErD'
+end
