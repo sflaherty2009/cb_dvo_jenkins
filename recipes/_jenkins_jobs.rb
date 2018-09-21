fac970f33e00e347875e258e585622c6b8e5c200
Scott Flaherty <scott_flaherty@trekbikes.com>
2018-09-21 11:20:39 -0500

    Added Jenkins job for domane builds

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,4 +1,4 @@
-jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh tbc_dev2 tbc_e2e maintenance_page)
+jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh tbc_dev2 tbc_e2e maintenance_page tbc_domane)
 
 jobs.each do |job|
   cookbook_file "#{Chef::Config[:file_cache_path]}/#{job}.xml" do
