7cb854a8d9ed0fdc47095f145e6f5241dfc43656
Scott Flaherty <scott_flaherty@trekbikes.com>
2018-09-25 13:15:30 -0500

    addition of maintenance page container build job.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,4 +1,4 @@
-jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh tbc_dev2 tbc_e2e maintenance_page tbc_domane tbc_test)
+jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh tbc_dev2 tbc_e2e maintenance_page tbc_domane tbc_test con_base_maintenance)
 
 jobs.each do |job|
   cookbook_file "#{Chef::Config[:file_cache_path]}/#{job}.xml" do
