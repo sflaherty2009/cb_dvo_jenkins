1a6149a33071efedd51234e094d0feaadda38466
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-01-11 09:30:31 -0600

    Adding Jenkins Agent Cleanup job. Please see change log for additional detail.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,4 +1,4 @@
-jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh maintenance_page con_base_maintenance con_base_apache con_base_centos con_base_freegeoip con_base_golang)
+jobs = %w(con_base_Hybris tbc dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh maintenance_page con_base_maintenance con_base_apache con_base_centos con_base_freegeoip con_base_golang Jenkins_Agent_Cleanup)
 
 jobs.each do |job|
   cookbook_file "#{Chef::Config[:file_cache_path]}/#{job}.xml" do
