b3b4f58ea788a8f2e0b3cd80684bade795402528
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-01-11 12:50:51 -0600

    pushing out changes for use with Chef Automate. Adding build_cookbook implementation.

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -4,3 +4,4 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::default'
+include_recipe 'cb_dvo_terraform::install'
--- .delivery/build_cookbook/recipes/provision.rb
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -4,3 +4,4 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::provision'
+include_recipe 'cb_dvo_terraform::acceptance' if workflow_stage?('acceptance')
