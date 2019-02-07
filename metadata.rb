0eb574ef4f64b1f976234582f236ca16f2f0ab05
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-02-07 15:59:33 -0600

    updating jenkins supermarket cookbook version to attempt to correct current cookbook error.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@trekbikes.com'
 license 'all_rights'
 description 'Installs/Configures jenkins resources with a focus on DVO Chef Pipeline needs.'
 long_description 'See README.md'
-version '0.2.46'
+version '0.2.47'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'centos'
 
@@ -12,7 +12,7 @@ source_url 'https://bitbucket.org/trekbikes/cb_dvo_jenkins'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_jeknins/issues?status=new&status=open'
 
 depends 'apt', '~> 6.1.4'
-depends 'jenkins', '~> 6.0.0'
+depends 'jenkins', '~> 6.2.1'
 depends 'java', '~> 1.50.0'
 depends 'firewall', '~> 2.6.2'
 depends 'poise-python', '~> 1.6.0'
