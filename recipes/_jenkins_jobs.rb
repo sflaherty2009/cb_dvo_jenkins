ad7b06e357645e3f4258703933156681a7324f80
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-03-04 15:00:41 -0600

    Updating traffic drain job, adding environment pinning and acr cleanup job. Adding further credentials for environment pinning job.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,4 +1,4 @@
-jobs = %w(con_base_Hybris dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh maintenance_page con_base_maintenance con_base_apache con_base_centos con_base_freegeoip con_base_golang Jenkins_Agent_Cleanup)
+jobs = %w(con_base_Hybris dvo_docker con_base_solr sonarqube dvo_wcpweb2 traffic_drain bump_web_heads database_refresh maintenance_page con_base_maintenance con_base_apache con_base_centos con_base_freegeoip con_base_golang Jenkins_Agent_Cleanup conhybris_acr_cleanup environment_pinning)
 
 jobs.each do |job|
   cookbook_file "#{Chef::Config[:file_cache_path]}/#{job}.xml" do
