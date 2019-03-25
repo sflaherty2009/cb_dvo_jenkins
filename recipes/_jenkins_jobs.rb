92a20d8887ec730178159bdf770771bebc136ba5
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-03-25 11:39:50 -0500

    trimming off the "files" from the job name, Can't determine why it's being added.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -2,14 +2,15 @@ jobs = run_context.cookbook_collection[:cb_dvo_jenkins].files_for('files')
 
 jobs.each do |job|
   filename = job['name']
-  cookbook_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
-    source filename
+  name = filename.tr('files/', '')
+  cookbook_file "#{Chef::Config[:file_cache_path]}/#{name}" do
+    source name
     action :create
   end
 
-  job_name = filename.tr('.xml', '')
+  job_name = name.tr('.xml', '')
 
   jenkins_job job_name do
-    config filename
+    config name
   end
 end
