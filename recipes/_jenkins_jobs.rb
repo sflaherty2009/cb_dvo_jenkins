79dbdf45de260bbc8e895f6a046012630e3cf50f
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-03-25 09:26:39 -0500

    previous call seems out of date and is not working appropriately, Trying different solution.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,14 +1,15 @@
-jobs = run_context.cookbook_collection[cookbook_name].template_filenames
+jobs = run_context.cookbook_collection['cookbook_name']
 
-jobs.each do |job|
-  cookbook_file "#{Chef::Config[:file_cache_path]}/#{job}" do
-    source job
+jobs.manifest['files'].each do |job|
+  filename = job['name']
+  cookbook_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
+    source filename
     action :create
   end
 
-  job_xml = File.join(Chef::Config[:file_cache_path], job)
+  job_xml = File.join(Chef::Config[:file_cache_path], filename)
 
-  jenkins_job job do
+  jenkins_job filename do
     config job_xml
   end
 end
