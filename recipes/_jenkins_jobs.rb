43f1585b609a8a12f2039f8d48a80e8f0011fab1
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-03-25 11:49:48 -0500

    correcting use of the trim command in jenkins_jobs

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -2,13 +2,13 @@ jobs = run_context.cookbook_collection[:cb_dvo_jenkins].files_for('files')
 
 jobs.each do |job|
   filename = job['name']
-  name = filename.tr('files/', '')
+  name = filename.sub('files/', '')
   cookbook_file "#{Chef::Config[:file_cache_path]}/#{name}" do
     source name
     action :create
   end
 
-  job_name = name.tr('.xml', '')
+  job_name = name.chomp('.xml')
 
   jenkins_job job_name do
     config name
