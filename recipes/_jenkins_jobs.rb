59bc39bcb5adc0af345d476bccc9fbe4636dbdb1
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-03-25 11:33:06 -0500

    running into issue with directory path in jenkins_job recipe.

--- recipes/_jenkins_jobs.rb
+++ recipes/_jenkins_jobs.rb
@@ -1,4 +1,4 @@
-jobs = run_context.cookbook_collection[:cb_dvo_jenkins].files_for('')
+jobs = run_context.cookbook_collection[:cb_dvo_jenkins].files_for('files')
 
 jobs.each do |job|
   filename = job['name']
